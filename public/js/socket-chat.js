7e4af814a02e0569479f17cb506b998214590891
Camilo <Camilo@Camilos-MacBook-Pro.local>
2021-01-20 11:17:07 -0500

    Chat ended

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,16 +1,16 @@
-var socket = io();
+var socket = io()
 
 var params = new URLSearchParams(window.location.search)
 
 if (!params.has('nombre') || !params.has('sala')) {
-    window.location = 'index.html'
+    window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios')
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
 
 
 
@@ -18,43 +18,33 @@ socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
-    })
+        renderizarUsuarios(resp)
+    });
+
 });
 
 // escuchar
 socket.on('disconnect', function() {
 
-    console.log('Perdimos conexión con el servidor');
+    console.log('Perdimos conexión con el servidor')
 
-});
-
-
-// Enviar información
-// socket.emit('crearMensaje', {
-//     nombre: 'Camilo',
-//     mensaje: 'Hola Mundo'
-// }, function(resp) {
-//     console.log('respuesta server: ', resp);
-// });
+})
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false)
+    scrollBottom()
 });
 
 // Escuchar cambios de usuarios
-// cuabdo un usuario entra o sale del chat
-
-socket.on('listaPersonas', function(personas) {
-
-    console.log('Servidor:', personas);
-
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    renderizarUsuarios(personas)
 });
 
-// mensajes provados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado: ', mensaje);
+
+    console.log('Mensaje Privado:', mensaje)
+
 })
